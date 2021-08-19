60968c8df843c9da9752cb4c3d07c5fe1a5a619d
galya-cmd <galya1985@bk.ru>
2021-08-20 00:30:21 +0300

    изменения

--- dictionary/src/App.js
+++ dictionary/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import'./components.dictionary.css'
+
 import Dictionary from './components/dictionary';
 
 function App() {
---
+++ dictionary/src/components/dictionary.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Dictionary(props) {
+    return(
+        <>
+        <button className="button">{props.name}</button>
+
+        </>
+    )
+}
+
+export default Dictionary;
