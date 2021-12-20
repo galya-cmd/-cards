63fe3ccee59cc4d3162101c406bcb49b49134baa
galya-cmd <galya1985@bk.ru>
2021-12-20 14:41:22 +0300

    лобавлены div в таблицу

--- dictionary/src/components/table.jsx
+++ dictionary/src/components/table.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import './table.css';
 
 class Table extends React.Component {
-  render(){
-  const {name,translation} = this.props;
-    return(
-    
-      <div className="table-cart">
-      <div>Слово</div>
-    <div>Транскрипция</div>
-    <div>Кнопки
-    <button>сохранить</button>
-        <button>редактировать</button>
-        <button>удалить</button>
-    </div>
-      </div>
-      );
-    }  
+    render() {
+        const { name, translation } = this.props;
+        return (
+
+            <div className="table-cart">
+                <div>Слово</div>
+                <div>{name}</div>
+                <div>Транскрипция</div>
+                <div>{translation}</div>
+                <div>Кнопки
+                    <button>сохранить</button>
+                    <button>редактировать</button>
+                    <button>удалить</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Table;
