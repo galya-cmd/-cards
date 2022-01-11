b7f6212f7c1caa0f402ab37141610b74b38b0277
galya-cmd <galya1985@bk.ru>
2022-01-11 22:34:15 +0300

    kfd

---
+++ dictionary/src/components/data.jsx
@@ -0,0 +1,33 @@
+export default [{
+    id: "10919",
+    name: 'horse',
+   transcription : '[hɔːs]',
+    translation:"лошадь" 
+    
+  },
+    {
+      id: "10920",
+    name: 'mouse',
+     transcription : '[maʊs]', 
+     translation:"мышь" 
+  }, 
+  {
+    id: "10921",
+    name: 'dragonfly', 
+    transcription : '[ˈdrægənflaɪ]', 
+    translation:"стрекоза" 
+  },
+  {
+    id: "10922",
+    name: "dog",
+  transcription: "[dɒg]",
+  translation: "собака",
+  },
+  {
+    id: "10924",
+    name: "rabbit",
+    transcription: "[ˈræbɪt]",
+    translation:"кролик",
+  
+    
+  }]
--- dictionary/src/components/header.jsx
+++ dictionary/src/components/header.jsx
@@ -4,7 +4,7 @@ function Header (){
         <>
         <nav className="menu-main">
         <ul>
-            <li><a href='/'>Table</a></li>
+            <li><a href='/table'>Table</a></li>
             <li><a href='/about'>About Us</a></li>
             <li><a href='/dictionary'>Dictionary</a></li>
             </ul>
